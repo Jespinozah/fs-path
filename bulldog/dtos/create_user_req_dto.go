e85bbd2ff76a67969ee308250bde9db1d19f7162
Brayan Espinoza <brayan.experity@experityhealth.com>
2025-04-06 15:52:15 -0500

    Migrate to bulldog folder

--- bulldog/dtos/create_user_req_dto.go
+++
@@ -1,8 +0,0 @@
-package dtos
-
-type CreateUserRequest struct {
-	Name     string `json:"name" binding:"required,min=1"`
-	Email    string `json:"email" binding:"required,email,min=1"`
-	Age      int    `json:"age" binding:"required,gt=0"`
-	Password string `json:"password" binding:"required,min=6"` // Password should be at least 6 characters
-}
--- bulldog/dtos/response_dto.go
+++
@@ -1,8 +0,0 @@
-package dtos
-
-type UserResponse struct {
-	ID    int    `json:"id"`
-	Name  string `json:"name"`
-	Email string `json:"email"`
-	Age   int    `json:"age"`
-}
